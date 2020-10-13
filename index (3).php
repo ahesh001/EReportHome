172ff7845831ce9df63b16a911e9d222beb505d5
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-10-13 12:00:22 -0400

    Index from C-Panel

---
+++ index (3).php
@@ -0,0 +1,36 @@
+<?php
+	require "header.php";
+?>
+
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<main>
+
+<section class="about" id="about">
+<h1> Who We Are</h1>
+<p> E-Report Phish is a fast growing startup with a mission to help alleviate the statics of successful phishing attacks on the college campus of State University of New York at Plattsburgh. We are comprised of a four person team of college seniors. Our members specialize in Front-End UI Design, Back-End DataBase, User Management & Policy, and Information Security Expertise. To find out more please click on <b>About</b></p>
+
+<h1> What We Offer</h1>
+<p> We offer a robust approach to phishing attack/attempts emails by having users sign up on our webpage then running them through a comprehensive training program where they are able to identify important fictitious emails. They will learn how to properly handle phishing emails, reporting phishing emails, and etc. For more information please check out the Traingings tab above. Our webiste offers a scan feature that allows users to scan any suspicious links or websites they may have come across. <br>
+<br>
+The scan tool is an API provided by Virus Total. and it runs the link through many top security industry vendors to verify the vaildity and safety of the link and/or website. We also have a database that users can search to check a domain or look up emails they have received in order to see if it is on our suspicious list because many of these attacks are reptitive and the same domain name might be used many times. We provide statistical data on how many emails we have logged, how many domain names they come from, and other key factors filed in a white paper report.
+ </p>
+
+<p class="text_column">Here will be a list of timestamps that each phishing email was detected and how often they&#39;re detected in emails.</p>
+</section>
+<h2>Tips & Tricks</h2>
+<body>Here are some tips on how to NOT be phished!</body>
+<p>1) You should ignore most "Free Money" emails.</p>
+<p>Add a lot more info</p>
+
+<h3>Take our quiz!</h3>
+<p>Quiz info here ??</p>
+
+</body>
+</html>
+<?php
+	require "footer.php";
+?>
