b3a3cd13d629360afefb2ba3068e9950f83b2665
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-10-13 12:06:19 -0400

    Login.php -cPanel

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+	require "header.php";
+?>
+<html>
+    
+    <title>Login</title>
+    <h1>Login to your account</h1>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+</html>
+
+<?php
+	require "footer.php";
+?>
