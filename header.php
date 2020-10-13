f8b4e27a9ca44182a9439543c64fc1487c89a1f1
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-10-13 12:04:53 -0400

    Header.php -cPanel

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+         
+<head><meta charset="utf-8"><meta name="description" content="This is an example!"><meta name=viewport content="width=device-width, initial scale=1">
+
+	<!-- <link rel="stylesheet" href="style.css"> -->
+</head>
+<body>
+<header>
+<p class="login-status" style="text-align: center;">&nbsp;<img alt="" src="/img/logo.png" style="width: 60px; height: 64px;" /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<a href="index.php">HOME |</a> <a href="about.php">ABOUT |</a> <a href="training.php">TRAINING |</a><a href="scan.php"> SCAN |</a> <a href="login.php">LOG IN |</a> <a href="signup.php">SIGN UP</a>&nbsp;</p>
+<br>
+<p class="login-status" style="text-align: center;"></p>
+
+<p class="login-status" style="text-align: center;"><span style="font-size:28px;"><span style="color:#800000;"><span class="e-report">E-REPORT</span> <span class="light">PHISH</span></span></span></p>
+
+<section class="hero" id="hero">
+<h2 class="tagline" style="font-style: italic; text-align: center;"><tt><big><span style="font-family:verdana,geneva,sans-serif;"><span style="font-size:12px;">Reporting phishing is as easy as fishing</span></span></big></tt></h2>
+</section>
+<br>
+</header>
+</body>
+</html>
