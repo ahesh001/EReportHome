63ae3a6ce69c51e7ec8506580d37061f865bfed9
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-10-17 06:46:22 -0400

    Training.php Cpanel
    CPanel code

---
+++ training.php
@@ -0,0 +1,64 @@
+<?php
+	require "header.php";
+?>
+<html>
+<head><meta charset="utf-8">
+	<title></title>
+</head>
+<body></body>
+</html>
+<p><br />
+<title></title>
+</p>
+
+<h1>Training</h1>
+
+<article class="footer_column">&nbsp;
+<p>Training Module Quiz:</p>
+
+<ol>
+	<li wfd-id="7"><span style="font-size:18px;"><span wfd-id="14">Which one is a possible sign of a Phishing E-mail?</span></span>
+
+	<ol>
+		<li wfd-id="12"><span style="font-size:18px;"><span wfd-id="13">Request for PII</span></span></li>
+		<li wfd-id="10"><span style="font-size:18px;"><span wfd-id="11">Blue color in email</span></span></li>
+		<li wfd-id="8"><span style="font-size:18px;"><span wfd-id="9">A message from faculty</span></span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="3"><span style="font-size:18px;">Which of the following is a phishing attack technique?</span>
+	<ol>
+		<li wfd-id="6"><span style="font-size:18px;">An embedded link</span></li>
+		<li wfd-id="5"><span style="font-size:18px;">A funny logo</span></li>
+		<li wfd-id="4"><span style="font-size:18px;">Digital signature on e-mail</span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="0"><span style="font-size:18px;">Which of these are <strong>ONE</strong> way to protect against getting phished?</span>
+	<ol>
+		<li wfd-id="2"><span style="font-size:18px;">Keep email app and antivirus software up-to-date</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Spend money on games</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Wear nice clothes</span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="1"><span style="font-size:18px;">Which one of the following is <u>best practice</u> if you encounter a phishing e-mail?</span>
+	<ol>
+		<li wfd-id="1"><span style="font-size:18px;"><strong>DO NOT</strong> click any links or attachments</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Click all links and attachments</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Send an e-mail to sender asking if they are trying to phish you</span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="1"><span style="font-size:18px;">&nbsp;How do you report a phishing e-mail to SUNY Plattsburgh?</span>
+	<ol>
+		<li wfd-id="1"><span style="font-size:18px;">Forward the email to @phishing.plattsburgh.edu</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Call the helpdesk</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">E-mail the college president</span></li>
+	</ol>
+	</li>
+</ol>
+</article>
+<!--?php
+	require "footer.php";
+?-->
