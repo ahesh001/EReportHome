67dab165647ab25173732db6153ae32de5c497e9
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-10-17 06:47:46 -0400

    Scan.php CPanel
    This documents contains the following code to our Scan feature provided by the Virus Total program

---
+++ scan (1).php
@@ -0,0 +1,30 @@
+<?php
+	require "header.php";
+?>
+
+<html>
+    
+    <title>Scanning</title>
+    <h1>Scan your sketchy websites!</h1>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</html>
+
+<?php
+	require "footer.php";
+?>
