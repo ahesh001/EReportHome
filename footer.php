c2d910a734a3151dc9d0e4184dd9e315ea67c897
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-10-17 06:51:04 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,13 +4,10 @@
 <section class="footer_banner" id="contact">
 
         
-<h1 class="hidden">Why Us</h1>
-
-<p> The need for our services is beyond great. Many of our peers have been victims of phishing emails and have lost their account or worse their idenity. We felt we had to take action and be the first to create a system like this for our school. We feel strongly that the work we are doing will have a big impact in flattening the curve of succesful phishing emails on our campus community.</p>
 
 <p class="hero_header">FOR THE LATEST NEWS &amp; UPDATES</p>
 
-<div class="button">subscribe</div>
+<button type="submit" name="Subscribe">Subscribe</button>
 </section>
 <!-- Copyrights Section -->
 
@@ -20,4 +17,3 @@
 </html>
     
 </footer>
-
