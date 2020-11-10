9bf483870a5ef09d6d7bb5df91209ffec196186b
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-11-10 06:08:07 -0500

    cPanel email.php

---
+++ email.php
@@ -0,0 +1,129 @@
+<?php
+    require "header.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="index.css" rel="stylesheet" /><meta charset="utf-8">
+	<title>Email Examples</title>
+</head>
+<body>
+<p><a href="/img/logo.png"><img alt="" src="/img/logo.png" style="width: 100px; height: 92px;" /></a></p>
+
+<p><span style="font-size:20px;"><span style="font-family:georgia,serif;"><strong>Lists of Identifiable Phishing Emails</strong></span></span></p>
+
+<p><tt><span style="font-size:18px;">1)</span></tt></p>
+
+<p style="text-align: justify;"><img alt="" src="/img/email screenshot 1.png" style="float: left; width: 500px; height: 250px;" /><span style="font-size:18px;"><tt>Subject: <span style="color:#FF0000;"><strong>US ARMY Recruitment</strong><b id="docs-internal-guid-ac7d956d-7fff-98ed-603c-7c5c8180540d">&nbsp;</b></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Sender:&nbsp;<span style="color:#FF0000;"><b id="docs-internal-guid-5fb88cd6-7fff-45f3-5a5c-c797b0a5ff0c">david</b><b>.c.dormann.mil@mail.mil</b></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Recipient: <span style="color:#FF0000;"><strong>A Plattsburgh Student</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Time:&nbsp;<span style="color:#FF0000;"><strong>9:42 A.M.</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Date:&nbsp;<span style="color:#FF0000;"><strong>Tuesday, May 19th</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Embedded Links: <span style="color:#FF0000;"><strong>No</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Suspected Reason: <span style="color:#FF0000;"><strong>To phish students into thinking they are talking to a military recruiter and joining the US ARMY just to collect PII.</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Description: An email sent to a SUNY Plattsburgh student claiming to offer a host of military benefits and awards if the recipient of email signs up 100% free tuition, monthly stipends, loan repayment, internship credits, etc all sound too good to be true. Most likely the goal is to get students PII data.</tt></span></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">2)</span></tt></p>
+
+<p style="text-align: justify;"><img alt="" src="/img/training screenshot 2.png" style="width: 500px; height: 216px; float: left;" /><span style="font-size:18px;"><tt>Subject:&nbsp;<span style="color:#FF0000;"><strong>Internship Placement Program</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Sender:&nbsp;<span style="color:#FF0000;"><strong>WONG0790@e.ntu.edu.sg via entuedu.onmicrosoft.com</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Recipient: <span style="color:#FF0000;"><strong>&gt;10 SUNY Plattsburgh Students</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Time:<span style="color:#FF0000;"> <strong>6:08 P.M.</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Date: <span style="color:#FF0000;"><strong>Monday September 23</strong> </span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Embedded Links: <span style="color:#FF0000;"><strong>Yes</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Suspected Reason:&nbsp;<span style="color:#FF0000;"><strong>To phish students into clicking on a malicious link that will deploy malware or that will ask users to input their PII and send it back.</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Description: An email sent to a number of SUNY Plattsburgh students claiming to offer an internship placement. Students are prompted to read attachment details by clicking on what appears to be &ldquo;Employment&rdquo; Description Word or Doc file. Most likely being a form of malware or chance to steal PII.</tt></span></p>
+
+<p><tt><span style="font-size:18px;">3)</span></tt></p>
+
+<p style="text-align: justify;"><img alt="" src="/img/email screenshot 3.png" style="width: 500px; height: 291px; float: left;" /><span style="font-size:18px;"><tt>Subject: <span style="color:#FF0000;"><strong>SUNY Plattsburgh Personal Assistant Position </strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Sender:&nbsp;<strong><span style="color:#FF0000;">kevin.marmo@siu.edu</span></strong></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Recipient:&nbsp;<strong><span style="color:#FF0000;">&gt; 10 SUNY Plattsburgh Students</span></strong></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Time: <span style="color:#FF0000;"><strong>8:27 A.M.</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Date: <span style="color:#FF0000;"><strong>Wednesday September 16</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Embedded Links: <span style="color:#FF0000;"><strong>Yes</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Suspected Reason:&nbsp;<span style="color:#FF0000;"><strong>To phish students into clicking on a malicious link that will deploy malware or that will ask users to input their PII and send it back.</strong></span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Description:&nbsp;An email sent to a number of SUNY Plattsburgh students claiming to offer a Personal Assistant Position. Students are prompted to read attachment details by clicking on what appears to be &ldquo;Employment&rdquo; Description Word or Doc file. Most likely being a form of malware.</tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>4)</tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt><img alt="" src="/img/email screenshot 4.png" style="width: 500px; height: 296px; float: left;" />Subject:&nbsp;<span style="color:#FF0000;">SUNY Plattsburgh Job and Internship Placement Fall 2020</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Sender:&nbsp;<span style="color:#FF0000;">taylie.payne@siu.edu</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Recipient: <span style="color:#FF0000;">&gt;10 SUNY Plattsburgh Students</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Time: <span style="color:#FF0000;">3:30 P.M.</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Date:&nbsp;<span style="color:#FF0000;">Monday August 31st</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Embedded Links: <span style="color:#FF0000;">Yes</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Suspected Reason:&nbsp;<span style="color:#FF0000;">To phish students into clicking on a malicious link that will deploy malware or that will ask users to input their PII and send it back.</span></tt></span></p>
+
+<p style="text-align: justify;"><span style="font-size:18px;"><tt>Description:&nbsp;An email sent to a number of SUNY Plattsburgh students claiming to offer a job opportunity and / or internship for the Fall 2020 semester. Students are prompted to read attachment details by clicking on what appears to be a &ldquo;Employment&rdquo; Description Word or Doc file. Most likely being a form of malware.</span></tt></span></p>
+
+<p style="text-align: justify;">5)</p>
+
+<p style="text-align: justify;"><img alt="" src="/img/email screenshot 5.png" style="width: 550px; height: 320px; float: left;" /><tt><span style="font-size:18px;">Subject:&nbsp;<span style="color:#FF0000;">SUNY&nbsp;Plattsburgh Job and Internship Summer Opportunities </span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Sender:&nbsp;<span style="color:#FF0000;">lucas.ackerman@siu.edu </span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Recipient:&nbsp;<span style="color:#FF0000;">&gt;10 SUNY Plattsburgh Students</span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Time:&nbsp;<span style="color:#FF0000;">11:45 A.M.&nbsp;</span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Date: <span style="color:#FF0000;">Wednesday August 12th</span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Embedded Links:&nbsp;<span style="color:#FF0000;">Yes</span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Suspected Reason:&nbsp;<span style="color:#FF0000;">To phish students into clicking on a malicious link that will deploy malware or that will ask users to input their PII and send it back.</span></span></tt></p>
+
+<p style="text-align: justify;"><tt><span style="font-size:18px;">Description:&nbsp;</span></tt></p>
+
+<p dir="ltr"><tt><span style="font-size:18px;">An email sent to a number of SUNY Plattsburgh students claiming to offer a job opportunity and / or internship for the Summer of 2020. Students are prompted to read attachment details by clicking on what appears to be &ldquo;Employment&rdquo; Description Word or Doc file. Most likely being a form of malware.</span></tt></span></p>
+
+<p></p>
+
+<p style="text-align: justify;"></p>
+
+<p style="text-align: right;">&nbsp;&nbsp;</p>
+
+<p style="text-align: right;"></p>
+
+<p style="text-align: right;"></p>
+
+<p dir="ltr"></p>
+
+<p style="text-align: right;"></p>
+
+<p style="text-align: right;"></p>
+
+<p dir="ltr"></p>
+
+<p style="text-align: right;"></p>
+</body>
+</html>
+<?php
+?>
