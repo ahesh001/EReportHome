e58f6675fb6244f24f79e2c6358d39c1d0cd8442
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-11-10 06:09:31 -0500

    Add files via upload

---
+++ scan (2).php
@@ -0,0 +1,32 @@
+<?php
+	require "header.php";
+?>
+<head>
+    <link rel="stylesheet" href="index.css">
+</head>
+<html>
+    
+    <title>Scanning</title>
+    <h1>Scan your sketchy websites!</h1>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</html>
+
+<?php
+	require "footer.php";
+?>
---
+++ training (1).php
@@ -0,0 +1,120 @@
+<?php
+	require "header.php";
+?>
+<html>
+<head><meta charset="utf-8">
+	<title>Training</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body></body>
+</html>
+<p><br />
+<title></title>
+</p>
+<p>Review our training slideshow module and then take the quiz below it.</p>
+<br>
+<h2>Training SlideShow Module</h2>
+<div class="mySlides fade">
+    <div class="numbertext">1 / 11</div>
+    <img src="/img/E-Report Phish Training Modules.png" style="width:100%">
+  </div>
+<div class="mySlides fade">
+    <div class="numbertext">2 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (1).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (2).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (3).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">5 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (4).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (5).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">7 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (6).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">8 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (7).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">9 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (8).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">10 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (9).png" style="width:100%">
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">11 / 11</div>
+    <img src="/img/E-Report Phish Training Modules (10).png" style="width:100%">
+  </div>
+  <br>
+  
+<h1>Training</h1>
+
+<article class="footer_column">&nbsp;
+<p>Training Module Quiz:</p>
+
+<ol>
+	<li wfd-id="7"><span style="font-size:18px;"><span wfd-id="14">Which one is a possible sign of a Phishing E-mail?</span></span>
+
+	<ol>
+		<li wfd-id="12"><span style="font-size:18px;"><span wfd-id="13">Request for PII</span></span></li>
+		<li wfd-id="10"><span style="font-size:18px;"><span wfd-id="11">Blue color in email</span></span></li>
+		<li wfd-id="8"><span style="font-size:18px;"><span wfd-id="9">A message from faculty</span></span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="3"><span style="font-size:18px;">Which of the following is a phishing attack technique?</span>
+	<ol>
+		<li wfd-id="6"><span style="font-size:18px;">An embedded link</span></li>
+		<li wfd-id="5"><span style="font-size:18px;">A funny logo</span></li>
+		<li wfd-id="4"><span style="font-size:18px;">Digital signature on e-mail</span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="0"><span style="font-size:18px;">Which of these are <strong>ONE</strong> way to protect against getting phished?</span>
+	<ol>
+		<li wfd-id="2"><span style="font-size:18px;">Keep email app and antivirus software up-to-date</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Spend money on games</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Wear nice clothes</span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="1"><span style="font-size:18px;">Which one of the following is <u>best practice</u> if you encounter a phishing e-mail?</span>
+	<ol>
+		<li wfd-id="1"><span style="font-size:18px;"><strong>DO NOT</strong> click any links or attachments</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Click all links and attachments</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Send an e-mail to sender asking if they are trying to phish you</span><br />
+		&nbsp;</li>
+	</ol>
+	</li>
+	<li wfd-id="1"><span style="font-size:18px;">&nbsp;How do you report a phishing e-mail to SUNY Plattsburgh?</span>
+	<ol>
+		<li wfd-id="1"><span style="font-size:18px;">Forward the email to @phishing.plattsburgh.edu</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">Call the helpdesk</span></li>
+		<li wfd-id="1"><span style="font-size:18px;">E-mail the college president</span></li>
+	</ol>
+	</li>
+</ol>
+</article>
+
+<h2>Screenshots</h2>
+<b><p> Please identify below whether or not this email is a phishing scam or not</p></b>
+<p style="text-align: center;"><b><img alt="" src="/img/training screenshot 1.png" style="width: 600px; height: 290px;" /> </b></p>
+<br>
+<p style="text-align: center;"><b><img alt="" src="/img/training screenshot 2.png" style="width: 600px; height: 290px;" /> </b></p>
+<br>
+<!--?php
+	require "footer.php";
+?-->
