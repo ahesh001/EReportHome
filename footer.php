0e4adcc298f89e0348d9023776f36df99912dd55
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-11-19 10:19:54 -0500

    Update footer.php
    Update from Cpanel

--- footer.php
+++ footer.php
@@ -1,19 +1,16 @@
 <footer>
-    
 <html>
+<!--
 <section class="footer_banner" id="contact">
-
-        
-
-<p class="hero_header">FOR THE LATEST NEWS &amp; UPDATES</p>
-
 <button type="submit" name="Subscribe">Subscribe</button>
 </section>
 <!-- Copyrights Section -->
-
+<!--
 <div class="copyright">&copy;2020- <strong>E- Report phish&nbsp;</strong></div>
 <!-- Main Container Ends -->
-    
+	<footer>
+		&copy; Copyright 2020. All Rights Reserved. <br>
+			<a href="mailto: Support@Ereport.bhweb.ws"> Support@Ereport.bhweb.ws</a>
+	</footer>
 </html>
-    
 </footer>
