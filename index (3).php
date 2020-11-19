80baa2e0eb0a7a17e20724df3c503b581822f2a6
ahesh001 <47195921+ahesh001@users.noreply.github.com>
2020-11-19 09:58:38 -0500

    Update index (3).php
    Update from CPanel site

--- index (3).php
+++ index (3).php
@@ -1,36 +1,40 @@
 <?php
-	require "header.php";
+    require "header.php";
 ?>
 
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
 <main>
-
+<h1>Intro</h1>
 <section class="about" id="about">
-<h1> Who We Are</h1>
-<p> E-Report Phish is a fast growing startup with a mission to help alleviate the statics of successful phishing attacks on the college campus of State University of New York at Plattsburgh. We are comprised of a four person team of college seniors. Our members specialize in Front-End UI Design, Back-End DataBase, User Management & Policy, and Information Security Expertise. To find out more please click on <b>About</b></p>
+<p>"Hello all, Did you know that phishing attacks remain one of the biggest dangers on the internet today?&quot; - Forbes&nbsp; Phishing emails are one of the many common attack vectors that scammers, criminals, and hackers use to breach the security of your email, computer, and/or network. The name of the game for these individuals is the art of imitation, their goal to fool(phish) you into believing that they are a legitimate person sometimes in the form of a boss, advisor, the government, PII like social security numbers, or clicking on dummy links that will infect your system and possibly the entire network with malware.</p>
 
-<h1> What We Offer</h1>
-<p> We offer a robust approach to phishing attack/attempts emails by having users sign up on our webpage then running them through a comprehensive training program where they are able to identify important fictitious emails. They will learn how to properly handle phishing emails, reporting phishing emails, and etc. For more information please check out the Traingings tab above. Our webiste offers a scan feature that allows users to scan any suspicious links or websites they may have come across. <br>
+<p style="text-align: center;"><b><img alt="" src="images/unknown.png" style="width: 200px; height: 190px;" /> </b></p>
+
+<h1><b>Who We Are</b></h1>
+
+<p>E-Report Phish is a fast growing startup with a mission to help alleviate the statics of successful phishing attacks on the college campus of State University of New York at Plattsburgh. We are comprised of a four person team of college seniors. Our members specialize in Front-End UI Design, Back-End DataBase, User Management &amp; Policy, and Information Security Expertise. To find out more please click on the <b>About</b> tab.<br></p>
+	
+	<p style="text-align: center;"><b><b><img alt="" src="images/kadiatou.jpg" style="width: 170px; height: 170px;" />&nbsp; &nbsp;<img alt="" src="images/kyle.jpg" style="width: 170px; height: 170px;" />&nbsp; &nbsp;<img alt="" src="images/Chris.png" style="width: 170px; height: 170px;" />&nbsp; &nbsp;<img alt="" src="images/Akeem.jpg" style="width: 170px; height: 170px;" /></b></b></p>
+<h1><b>What We Offer</b></h1>
+
+<p>We offer a robust approach to phishing attack/attempts emails by having users sign up on our webpage then running them through a comprehensive training program where they are able to identify important fictitious emails. They will learn how to properly handle phishing emails, reporting phishing emails, and etc. For more information please check out the Traingings tab above. Our webiste offers a scan feature that allows users to scan any suspicious links or websites they may have come across.<br />
 <br>
-The scan tool is an API provided by Virus Total. and it runs the link through many top security industry vendors to verify the vaildity and safety of the link and/or website. We also have a database that users can search to check a domain or look up emails they have received in order to see if it is on our suspicious list because many of these attacks are reptitive and the same domain name might be used many times. We provide statistical data on how many emails we have logged, how many domain names they come from, and other key factors filed in a white paper report.
- </p>
+The scan tool is an API provided by Virus Total. and it runs the link through many top security industry vendors to verify the vaildity and safety of the link and/or website. We also have a database that users can search to check a domain or look up emails they have received in order to see if it is on our suspicious list because many of these attacks are reptitive and the same domain name might be used many times. We provide statistical data on how many emails we have logged, how many domain names they come from, and other key factors filed in a white paper report.</p>
 
 <p class="text_column">Here will be a list of timestamps that each phishing email was detected and how often they&#39;re detected in emails.</p>
-</section>
-<h2>Tips & Tricks</h2>
-<body>Here are some tips on how to NOT be phished!</body>
-<p>1) You should ignore most "Free Money" emails.</p>
-<p>Add a lot more info</p>
 
-<h3>Take our quiz!</h3>
-<p>Quiz info here ??</p>
+<p class="text_column" style="text-align: center;"><b><img alt="" src="images/training.png" style="width: 300px; height: 92px;" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<img alt="" src="images/unnamed.png" style="width: 300px; height: 176px;" /></b></p>
+
+<p class="text_column"></p>
+	
+</section>
+
+<h1>Why Us?</h1>
+
+<p>The need for our Services is beyond great. Many of our peers have been victims of phishing emails and have lost their account or worse their identity. We felt we had to take action and be the first to create a system like this for our school. We feel strongly that the work we are doing will have a big impact in flattening the curve of successful phishing emails on our campus community.</p>
 
 </body>
 </html>
+
 <?php
-	require "footer.php";
+    require "footer.php";
 ?>
